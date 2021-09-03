da1b28c1a446c6860bda1a8840b9ad2fe829b7c8
Anton Voloshyn <antonvoloshyn@Antons-MacBook-Pro.local>
2021-09-03 14:12:38 +0300

    add axios

---
+++ src/config/RestConfig.tsx
@@ -0,0 +1,2 @@
+export const url = "https://newsapi.org/v2/everything?q=tesla&from=2021-08-03&sortBy=publishedAt&apiKey=ca9dbb585bf54ea6a04a51bc7b48b2df"
+
--- src/screens/ItemScreen.tsx
+++ src/screens/ItemScreen.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { Component, useState, useEffect } from "react";
 import {
   Text,
   View,
@@ -10,6 +10,67 @@ import {
   TouchableOpacity,
   ActivityIndicator,
 } from "react-native";
+import { Articles } from "../interfaces/NewsInterface";
+import axios, { AxiosResponse } from "axios";
+
+const Item = ({ title }: { title: any }) => (
+  <TouchableOpacity onPress={() => Alert.alert(title)}>
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  </TouchableOpacity>
+);
+
+function ItemScreen() {
+  const [newsDate, setNewsDate] = useState([]);
+  useEffect(() => {
+    axios
+      .get<Articles>(
+        "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=ca9dbb585bf54ea6a04a51bc7b48b2df"
+      )
+      .then((response: AxiosResponse) => {
+        setNewsDate(response.data);
+      });
+  }, []);
+
+  console.log("NewsDate: ", newsDate);
+  console.log("NewsLegth: ", newsDate.length);
+
+  const renderItem = ({ item }: { item: any }) => <Item title={item.title} />;
+
+  return (
+    // <SafeAreaView style={styles.container}>
+    //   <FlatList
+    //     data={newsDate}
+    //     renderItem={renderItem}
+    //     keyExtractor={(item) => item.source.id}
+    //   />
+    // </SafeAreaView>
+    <SafeAreaView>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>{newsDate[0].title}</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: "#c8ccc9",
+    elevation: 5,
+    borderRadius: 15,
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
 
 const DATA = [
   {
@@ -72,46 +133,4 @@ const DATA = [
     age: "8",
   },
 ];
-
-
-const Item = ({ title }: { title: any }) => (
-  <TouchableOpacity onPress={() => Alert.alert(title)}>
-    <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
-    </View>
-  </TouchableOpacity>
-);
-
-function ItemScreen() {
-  const renderItem = ({ item }: { item: any }) => <Item title={item.title} />;
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: "#c8ccc9",
-    elevation: 5,
-    borderRadius: 15,
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 32,
-  },
-});
-
 export default ItemScreen;
